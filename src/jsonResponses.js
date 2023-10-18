b558e0ea28da978f9811e1289b1c7de64fb0a8bc
Shenchengfu <sf2559@g.rit.edu>
2023-10-17 21:16:14 -0400

    1

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -12,62 +12,63 @@ const getCardInfo = async (request, response) => {
   const parsedUrl = urlParser.parse(request.url);
   const params = query.parse(parsedUrl.query);
 
-  let url = 'https://db.ygoprodeck.com/api/v7/cardinfo.php?';
+  let url = 'https://db.ygoprodeck.com/api/v7/cardinfo.php?'
   // allow get/head request
   if (request.method === 'GET' || request.method === 'HEAD') {
-    const {
-      fname, type, num, offset,
-    } = params;
+    const { fname, type, num, offset } = params
     if (!fname || !type) {
-      const responseJSON = {
+      const responseJSON1 = {
         id: 'badRequest',
-        message: 'Missing fname or type query parameter',
-      };
-      return respondJSON(request, response, 400, responseJSON);
+        message: 'Missing fname or type query parameter'
+      }
+      return respondJSON(request, response, 400, responseJSON1);
     }
-    url = `${url}fname=${fname}&type=${type}&num=${num}&offset=${offset}`;
+    url = `${url}fname=${fname}&type=${type}&num=${num}&offset=${offset}`
 
     try {
       const res = await axios.get(url);
-      const { data } = res;
+      const data = res.data;
       return respondJSON(request, response, 200, data);
     } catch (error) {
-      console.error(error.response.status);
       if (error.response.status === 400) {
-        const responseJSON = {
+        const responseJSON2 = {
           id: 'badRequest',
-          message: error.response.data.error,
-        };
-        return respondJSON(request, response, 400, responseJSON);
+          message: error.response.data.error
+        }
+        return respondJSON(request, response, 400, responseJSON2);
       }
-      const responseJSON = {
+      const responseJSON3 = {
         message: 'Internal Server Error. Something went wrong',
         id: 'internalError',
       };
-      return respondJSON(request, response, 500, responseJSON);
+      return respondJSON(request, response, 500, responseJSON3);
     }
   }
   // allow post request
   if (request.method === 'POST') {
-    const params = [];
-    request.on('data', (chunk) => {
-      params.push(chunk);
-    });
+    let body = []
+    request.on('data', chunk => {
+      body.push(chunk)
+    })
     request.on('end', async () => {
       try {
-        const res = await axios.post(url, params);
-        const { data } = res;
+        const res = await axios.post(url, body);
+        const data = res.data;
         return respondJSON(request, response, 200, data);
       } catch (error) {
-        console.error(error);
-        const responseJSON = {
+        const responseJSON4 = {
           message: 'Internal Server Error. Something went wrong',
           id: 'internalError',
         };
-        return respondJSON(request, response, 500, responseJSON);
+        return respondJSON(request, response, 500, responseJSON4);
       }
-    });
+    })
   }
+  const responseJSON5 = {
+    message: 'do not support other request method',
+    id: 'internalError',
+  };
+  return respondJSON(request, response, 500, responseJSON5);
 };
 
 const notFound = (request, response) => {
--- src/server.js
+++ src/server.js
@@ -7,31 +7,31 @@ const jsonHandler = require('./jsonResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-  CLIENT: {
-    '/': htmlHandler.getIndex,
-    '/script.js': htmlHandler.getJS,
-    '/style.css': htmlHandler.getCSS,
-    '/logo.png': htmlHandler.getLogo,
-  },
-  JSON: {
-    '/getCardInfo': jsonHandler.getCardInfo,
-    notFound: jsonHandler.notFound,
-  },
+    CLIENT: {
+        '/': htmlHandler.getIndex,
+        '/script.js': htmlHandler.getJS,
+        '/style.css': htmlHandler.getCSS,
+        '/logo.png': htmlHandler.getLogo,
+    },
+    JSON: {
+        '/getCardInfo': jsonHandler.getCardInfo,
+        notFound: jsonHandler.notFound,
+    },
 };
 
 const onRequest = (request, response) => {
-  const parsedUrl = url.parse(request.url);
+    const parsedUrl = url.parse(request.url);
 
-  if (urlStruct.CLIENT[parsedUrl.pathname]) {
-    return urlStruct.CLIENT[parsedUrl.pathname](request, response);
-  }
-  if (urlStruct.JSON[parsedUrl.pathname]) {
-    return urlStruct.JSON[parsedUrl.pathname](request, response);
-  }
+    if (urlStruct.CLIENT[parsedUrl.pathname]) {
+        return urlStruct.CLIENT[parsedUrl.pathname](request, response);
+    }
+    if (urlStruct.JSON[parsedUrl.pathname]) {
+        return urlStruct.JSON[parsedUrl.pathname](request, response);
+    }
 
-  return urlStruct.JSON.notFound(request, response);
+    return urlStruct.JSON.notFound(request, response);
 };
 
 http.createServer(onRequest).listen(port, () => {
-  console.log(`Listening on http://127.0.0.1:${port}`);
+    // console.log(`Listening on http://127.0.0.1:${port}`);
 });
